5b30a3ed6349e01353951d064dee2ea4db1cf9c9
Dominic S <domijschweizer@gmail.com>
2026-05-29 14:40:48 +0200

    new harness pic and better softshells

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
     this.config.imageLink2 = image[0];
     this.pickedColor = image[1];
   }
-  colors2: [string, string][] = ['christmas-2', 'christmas', 'clouds', 'knots', "knots2","knots3","knots4",  'mosaik', 'mosaik2', 'snowflakes', 'stars'].map(s => [`assets/colors2/${s}.PNG`, s])
+  colors2: [string, string][] = ['christmas', 'deer', 'knots', 'mosaik', 'pattern', 'stars', 'winter'].map(s => [`assets/colors/${s}-small.png`, s])
   URLSoftShell: [string, string][] = [
     ...this.colors2,
     
